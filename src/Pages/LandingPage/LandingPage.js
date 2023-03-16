9c03f2b3faa549e512caee198bb6eaaab988e734
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-03-16 20:06:57 +0600

    create react app

---
+++ src/Pages/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from './About'
+import Banner from './Banner'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Banner/>
+        <About/>
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ src/Pages/Routes/Router/Router.js
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../../layout/Main/Main";
+import ErrorPage from "../../ErrorPage/ErrorPage";
+import LandingPage from "../../LandingPage/LandingPage";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <LandingPage></LandingPage>,
+        },
+        {
+          path: "/home",
+          element: <LandingPage></LandingPage>,
+        },
+      ],
+    },
+    {
+        path: "*",
+        element: <ErrorPage> </ErrorPage>,
+    },
+  ]);
+  export default router;
