fa29e0024e4937221ba2a14fee9e94c2471425cd
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-03-17 17:52:48 +0600

    single user page

--- src/Pages/UserDetailsPage/UserDetailsPage.js
+++ src/Pages/UserDetailsPage/UserDetailsPage.js
@@ -1,21 +1,80 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom'
-import userImg from '../../media/defaultUser.png'
+import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import userImg from "../../media/defaultUser.png";
 
 const UserDetailsPage = () => {
-    const user = useLoaderData()
-    const {name} = user
+  const user = useLoaderData();
+  console.log(user);
+  const { name, username, email, phone, website, company, address } = user;
   return (
-    <div className='py-20'>
-        <div className=' w-3/4 mx-auto grid grid-cols-3 gap-5'>
-           <div className='bg-[#bb202d24] text-center'>
-           <img src={userImg} alt=""  className=' rounded-full border-4 border-[#692438] w-28 mx-auto'/>
-           <p>{name}</p>
-           </div>
-           <div className='bg-[#bb202d24] col-span-2'></div>
+    <div className=" min-h-screen py-20 bg-[#F1F3FA]">
+        
+      <div className=" w-3/4 mx-auto ">
+        <div className="mb-10">
+        <Link to="/users" target="_blank">
+            <button className="mt-10 px-10  bg-[#bb202e] text-white py-2 border-2 border-[#bb202e] hover:bg-white hover:text-[#bb202e] duration-500 rounded-md">
+              Go Back to Previous Page
+            </button>
+          </Link>
         </div>
+        <div className="grid grid-cols-3 gap-5">
+        <div className="bg-[#ffffff] text-center py-10 rounded-lg">
+          <img
+            src={userImg}
+            alt=""
+            className=" rounded-full border-4 border-[#692438] w-28 mx-auto"
+          />
+          <h3 className="mt-3 text-2xl font-bold">{name}</h3>
+          <p className=" mt-2 text-[#692438] text-xl font-bold">{username}</p>
+          <p className=" mt-4 text-gray-500 text-base font-bold">
+            <span>{address.suite},</span>
+            <span>{address.street},</span>
+            <span>{address.city}</span>
+          </p>
+          <Link to={`https://www.${website}`} target="_blank">
+            <button className="mt-10 w-4/5 bg-[#bb202e] text-white py-2 border-2 border-[#bb202e] hover:bg-white hover:text-[#bb202e] duration-500 rounded-md">
+              Visit Website
+            </button>
+          </Link>
+        </div>
+        <div className="bg-[#ffffff] col-span-2 rounded-lg">
+            <p className="p-4 border-b-2">
+                <span className=" font-bold">Full Name: </span>
+                <span>{name}</span>
+            </p>
+            <p className="p-4 border-b-2">
+                <span className=" font-bold">Email: </span>
+                <span>{email}</span>
+            </p>
+            <p className="p-4 border-b-2">
+                <span className=" font-bold">Phone Number: </span>
+                <span>{phone}</span>
+            </p>
+            <p className="p-4 border-b-2">
+                <span className=" font-bold">Website: </span>
+                <span>{website}</span>
+            </p>
+            <p className="p-4 border-b-2">
+                <span className=" font-bold">Company Name: </span>
+                <span>{company.name}</span>
+            </p>
+            <p className="p-4 border-b-2">
+                <span className=" font-bold">Catchphrase: </span>
+                <span>{company.catchPhrase}</span>
+            </p>
+            <p className="p-4 border-b-2">
+                <span className=" font-bold">Address: </span>
+                <span>
+                <span>{address.suite},</span>
+            <span>{address.street},</span>
+            <span>{address.city}</span>
+                </span>
+            </p>
+        </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserDetailsPage
+export default UserDetailsPage;
--- src/Pages/UsersPage/UsersPage.js
+++ src/Pages/UsersPage/UsersPage.js
@@ -6,7 +6,7 @@ const UsersPage = () => {
   const users = useLoaderData();
   console.log(users);
   return (
-    <div className="py-20 bg-[#ffffff]">
+    <div className="py-20 bg-[#F1F3FA]">
       <div className="w-[100%] sm:w-4/5  pt-10 mx-auto border-t-[3px] border-[#bb202e]">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-10">
           {users.map((user) => (
